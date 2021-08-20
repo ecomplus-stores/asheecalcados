5b5e84fccf01cd6950e6ec0c35995d5f363cec2a
projsisweb <projetos@sisweb.app>
2021-08-20 10:07:50 -0300

    cabeçalho

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,11 @@
 // Add your custom JavaScript for storefront pages here.
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("header").style.opacity = "1";
+  } else {
+    document.getElementById("header").style.opacity = ".5";
+  }
+}
