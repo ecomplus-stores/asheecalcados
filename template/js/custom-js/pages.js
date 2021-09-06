6e085cc4d59d478b3e22da11cd6dfda245cbba8d
projsisweb <projetos@sisweb.app>
2021-09-05 21:47:17 -0300

    imagem superior

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,30 @@
 // Add your custom JavaScript for storefront pages here.
+
+$(window).ready(function () {
+
+    if (window.matchMedia('screen and (min-width: 700px)').matches) {
+
+        if (window.location.pathname === "/") {
+            $('#header').css('opacity', '0.5');
+        }
+    }
+
+});
+
+
+$(window).scroll(function () {
+
+    if (window.matchMedia('screen and (min-width: 700px)').matches) {
+        if (window.location.pathname === "/") {
+            $('#header').css('opacity', '0.5');
+        }
+    }
+
+    if ($(window).scrollTop() > 0) {
+        $('#header').css('opacity', '1.0');
+    }
+});
+
+$("#header").click(function () {
+    $('#header').css('opacity', '1.0');
+});
